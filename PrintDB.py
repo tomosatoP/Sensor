15abae97a7b9537db4c031c7e6c99a8bd592da57
tomosatoP <tomosato.g@gmail.com>
2023-06-03 18:21:08 +0900

    2023-06-03

--- PrintDB.py
+++ PrintDB.py
@@ -5,7 +5,7 @@ import sqlite3
 from datetime import datetime, timedelta
 
 
-def latestdate(db_name: str, table_name: str):
+def latestdate(db_name: str):
     with sqlite3.connect(db_name) as cn:
         cc = cn.cursor()
 
@@ -13,14 +13,14 @@ def latestdate(db_name: str, table_name: str):
         base_date = datetime.now()
         select_date = (base_date - range_date).isoformat(timespec="seconds")
 
-        db_command_select = f"SELECT * FROM {table_name} WHERE DateTime > ?"
+        db_command_select = "SELECT * FROM condition WHERE DateTime > ?"
         cc.execute(db_command_select, (select_date,))
         for row in cc.fetchall():
             print(row)
 
 
 if __name__ == "__main__":
-    latestdate("cpu.sqlite3", "cpu")
-    latestdate("extbme280.sqlite3", "extbme280")
-    latestdate("mntbme280.sqlite3", "mntbme280")
-    latestdate("mnttsl2572.sqlite3", "mnttsl2572")
+    latestdate("cpu.sqlite3")
+    latestdate("extbme280.sqlite3")
+    latestdate("mntbme280.sqlite3")
+    latestdate("mnttsl2572.sqlite3")
--- prototype.py
+++ prototype.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import logging
 import sqlite3
 from configparser import ConfigParser
@@ -7,6 +8,7 @@ from datetime import datetime
 from subprocess import PIPE, Popen
 
 from cgsensor import BME280, TSL2572  # type: ignore
+from smb.SMBConnection import SMBConnection as NAS
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -25,13 +27,15 @@ class SENSORError(Exception):
 
 
 class _sensor:
-    cpu_table: list
-    bme280_table: list
-    tsl2572_table: list
+    table: list = list()
+    registered_sensornames: list = list()
+
+    backup_ip: str
+    backup_service_name: str
+    backup_path: str
 
     def __init__(self, name: str) -> None:
         self.status: bool = True
-        self.tablelist: list
         self.name: str = name
         self.data: dict = dict()
 
@@ -47,31 +51,55 @@ class _sensor:
     def store(self) -> bool:
         self.status = False
 
-        db_command_create: list = ["CREATE TABLE IF NOT EXISTS"]
-        db_command_create.append(self.name)
-        db_command_create.append("(")
-        db_command_create.append(",".join(self.tablelist))
-        db_command_create.append(")")
+        try:
+            db_command_create: list = ["CREATE TABLE IF NOT EXISTS"]
+            db_command_create.append("condition")
+            db_command_create.append("(")
+            db_command_create.append(",".join(self.table))
+            db_command_create.append(")")
 
-        values: tuple = tuple([self.now] + list(self.data.values()))
-        db_command_insert: list = ["INSERT INTO"]
-        db_command_insert.append(self.name)
-        db_command_insert.append("VALUES(")
-        db_command_insert.append(",".join(["?"] * len(values)))
-        db_command_insert.append(")")
+            values: tuple = tuple([self.now] + list(self.data.values()))
+            db_command_insert: list = ["INSERT INTO"]
+            db_command_insert.append("condition")
+            db_command_insert.append("VALUES(")
+            db_command_insert.append(",".join(["?"] * len(values)))
+            db_command_insert.append(")")
 
-        with sqlite3.connect(database=f"{self.name}.sqlite3") as cn:
-            cc = cn.cursor()
-            cc.execute(" ".join(db_command_create))
-            cc.execute(" ".join(db_command_insert), values)
+            with sqlite3.connect(database=f"{self.name}.sqlite3") as cn:
+                cc = cn.cursor()
+                cc.execute(" ".join(db_command_create))
+                cc.execute(" ".join(db_command_insert), values)
 
-            self.status = True
+                self.status = True
 
-        return self.status
+        except sqlite3.DatabaseError as err:
+            raise SENSORError((f"{self.__class__.__name__} Store", err))
+
+        except OSError as err:
+            raise SENSORError((f"{self.__class__.__name__} Store", err))
+
+        finally:
+            return self.status
 
     def backup(self) -> bool:
         self.status = False
-        return self.status
+
+        try:
+            conn = NAS("NA", "NA", "NA", "NA", is_direct_tcp=True)
+            conn.connect(ip=self.backup_ip, port=445)
+
+            with open(file=f"{self.name}.sqlite3", mode="rb") as f:
+                conn.storeFile(self.backup_service_name, f"{self.backup_path}/{self.name}.sqlite3", f)
+
+            conn.close()
+
+            self.status = True
+
+        except OSError as err:
+            raise SENSORError((f"{self.__class__.__name__} Backup", err))
+
+        finally:
+            return self.status
 
 
 class SensorCpu(_sensor):
@@ -96,12 +124,6 @@ class SensorCpu(_sensor):
 
         return self.status
 
-    def store(self) -> bool:
-        self.tablelist = self.cpu_table
-        super().store()
-
-        return self.status
-
 
 class SensorBme280(_sensor):
     def __init__(self, name: str) -> None:
@@ -116,8 +138,9 @@ class SensorBme280(_sensor):
             self.sensor = BME280(self.i2c_address)
             self.status = self.sensor.check_id()
             logger.info(f"BME280 {self.i2c_address:#x} check id: {self.status}")
+
         except IOError as ioerr:
-            raise SENSORError(ioerr)
+            raise SENSORError((f"{self.__class__.__name__} Check", ioerr))
 
         finally:
             return self.status
@@ -135,16 +158,10 @@ class SensorBme280(_sensor):
                 logger.warning(f"Sensor {self.name} did not function properly.")
 
         except IOError as ioerr:
-            raise SENSORError(ioerr)
+            raise SENSORError((f"{self.__class__.__name__} Measure", ioerr))
 
         finally:
             return self.status
-
-    def store(self) -> bool:
-        self.tablelist = self.bme280_table
-        super().store()
-
-        return self.status
 
 
 class SensorTsl2572(_sensor):
@@ -162,7 +179,7 @@ class SensorTsl2572(_sensor):
             logger.info(f"TSL2572 {self.i2c_address:#x} check id: {self.status}")
 
         except IOError as ioerr:
-            raise SENSORError(ioerr)
+            raise SENSORError((f"{self.__class__.__name__} Check", ioerr))
 
         finally:
             return self.status
@@ -178,51 +195,48 @@ class SensorTsl2572(_sensor):
                 logger.warning(f"Sensor {self.name} did not function properly.")
 
         except IOError as ioerr:
-            raise SENSORError(ioerr)
+            raise SENSORError((f"{self.__class__.__name__} Measure", ioerr))
 
         finally:
             return self.status
 
-    def store(self) -> bool:
-        self.tablelist = self.tsl2572_table
-        super().store()
-
-        return self.status
-
 
 def config(sensor_type: str) -> list:
-    result: list = list()
-
     def str2list(target: str, sep: str = ",") -> list:
         return list(map(lambda x: x.strip(), target.split(sep=sep)))
 
     config = ConfigParser(converters={"list": str2list})
     config.read("config.ini")
 
-    result = config.getlist(sensor_type, "sensorlist")  # type: ignore
-    _sensor.cpu_table = config.getlist(sensor_type, "cputable")  # type: ignore
-    _sensor.bme280_table = config.getlist(sensor_type, "bme280table")  # type: ignore
-    _sensor.tsl2572_table = config.getlist(sensor_type, "tsl2572table")  # type: ignore
+    result: list = config.getlist(sensor_type, "sensorlist")  # type: ignore
+
+    _sensor.backup_ip = config.get(sensor_type, "ip")
+    _sensor.backup_service_name = config.get(sensor_type, "service_name")
+    _sensor.backup_path = config.get(sensor_type, "path")
+
+    SensorCpu.table = config.getlist(sensor_type, "cputable")  # type: ignore
+    SensorBme280.table = config.getlist(sensor_type, "bme280table")  # type: ignore
+    SensorTsl2572.table = config.getlist(sensor_type, "tsl2572table")  # type: ignore
+
+    SensorCpu.registered_sensornames = ["cpu"]
+    SensorBme280.registered_sensornames = ["extbme280", "mntbme280"]
+    SensorTsl2572.registered_sensornames = ["mnttsl2572"]
 
     return result
 
 
 if __name__ == "__main__":
     # "DEFAULT", "PRZWO" ,"ADRWB", "ADRWO"
-    sensors: list = config("DEFAULT")
+    sensornames: list = config("DEFAULT")
 
-    sensor: list = list()
-    for sensorname in sensors:
-        if sensorname == "cpu":
-            sensor.append(SensorCpu(sensorname))
-        elif sensorname in ["extbme280", "mntbme280"]:
-            sensor.append(SensorBme280(sensorname))
-        elif sensorname == "mnttsl2572":
-            sensor.append(SensorTsl2572(sensorname))
+    sensors: list = list()
+    for sensorname in sensornames:
+        for sensorclass in _sensor.__subclasses__():
+            if sensorname in sensorclass.registered_sensornames:
+                sensors.append(sensorclass(sensorname))
 
-    for s in sensor:
-        if s.check():
-            s.measure()
-            s.store()
-            s.backup()
-            print(s.data)
+    for sensor in sensors:
+        if sensor.check():
+            sensor.measure()
+            sensor.store()
+            sensor.backup()
